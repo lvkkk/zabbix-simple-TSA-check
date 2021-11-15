386340a0d323acd3bdda57af8dd6cab1d76c0395
lvkkk <48206914+lvkkk@users.noreply.github.com>
2021-11-15 11:09:13 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "tsa check" | openssl ts -query -no_nonce -sha1 2>/dev/null | curl -s -S -H 'Content-Type: application/timestamp-query' --data-binary @- http://tsa.example.com/ -o response.tsr
-python3 sndzb.py "`openssl ts -reply -text -in response.tsr 2>/dev/null | grep Time`"
+python3 /usr/share/tsacheck/sndzb.py "`openssl ts -reply -text -in response.tsr 2>/dev/null | grep -oPi \"(?<=Time stamp: ).*\"`"
